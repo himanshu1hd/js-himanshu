7c29c606a06907543c6d4497cc06e6a75c8a9c0e
himanshu1hd <himanshudhepe1@gmail.com>
2024-12-09 06:18:51 +0000

    conversion of datatypes

---
+++ 01_basics/03_Conversion.js
@@ -0,0 +1,24 @@
+let score=undefined
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber=Number(score) //conversion
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+
+//"33"=>33
+//"33asd"=>NaN
+//true=>1;false=>0
+
+let isLoggedIn=1;
+
+//1=>true;0=>false
+//""=>false
+//"hitesh"=>true
+
+let somenumber=33;
+
+let stringnumber=String(somenumber)
+console.log(typeof(stringnumber));
